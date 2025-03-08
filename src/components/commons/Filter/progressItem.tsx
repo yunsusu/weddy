e9dbe49221b4b8a48fed926817db07037de4cca4
yunsusu <rladbstn1212@gmail.com>
2025-03-08 20:03:51 +0900

    fix : 디자인 수정

--- src/components/commons/Filter/progressItem.tsx
+++ src/components/commons/Filter/progressItem.tsx
@@ -1,3 +1,7 @@
+import classNames from "classnames/bind";
+import styles from "./style.module.scss";
+
+const cn = classNames.bind(styles);
 export default function ProgressItem({ item, func, selectedStatus }: any) {
   return (
     <div>
@@ -6,6 +10,7 @@ export default function ProgressItem({ item, func, selectedStatus }: any) {
         id={item}
         name="progressStatus"
         value={item}
+        className={cn("inputCheck")}
         checked={selectedStatus === item} // 선택된 값과 같으면 체크
         onChange={() => func(item)}
       />
